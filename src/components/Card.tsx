e2113fdebaf826dd3d426174808a37038c787867
visionary <lwazijakuja0@gmail.com>
2024-12-21 15:05:10 +0200

    added card ackground image

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -24,14 +24,9 @@ function Card({display, hide, isFlipped, index, HandleClick}:Props) {
     <div className={`card group perspective`} >
         {/* <img src="https://picsum.photos/45" /> */}
       <div className={`absolute w-[70px] h-[70px] lg:w-[120px] lg:h-[90px] cursor-pointer transition-transform duration-500 transform ${Flipped ? 'rotate-y-180' : ''} ${hide}`} onClick={HandleClick}>
-        {
-          Flipped ? 
-          <img onError={()=> {false}} src={display} className={'back'} loading="eager"  />
-          :
-          <div className="front">
-            {index}
-          </div>
-        }
+        <div className="front">
+          <img onError={()=> {false}} src={Flipped ? display : "/icons/card-bg.jpeg"} className={'back'} loading="eager" />  
+        </div>
       </div>
     </div>
   )
